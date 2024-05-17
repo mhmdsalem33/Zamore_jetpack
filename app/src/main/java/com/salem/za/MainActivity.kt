606d9405b6aaf47be787a5770173ce12fbead65f
mhmd salem <mhmd.salem33@yahoo.com>
2024-05-17 15:04:44 +0300

    adding app icon logo

--- app/src/main/java/com/salem/za/MainActivity.kt
+++ app/src/main/java/com/salem/za/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : ComponentActivity() {
      
         
         installSplashScreen().setKeepOnScreenCondition{
-            true
+            false
         }
 
         super.onCreate(savedInstanceState)
